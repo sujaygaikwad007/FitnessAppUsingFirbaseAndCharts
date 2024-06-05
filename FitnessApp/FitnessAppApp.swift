79c8f501e64c7cd1a9da933e8cefbe429dab053f
Aniket Patil <aniket.p@reapmind.com>
2024-06-05 15:11:10 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Aniket Patil on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
